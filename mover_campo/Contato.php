eea74e432ee35fcee05dff000b35ab5f6bf1f3d7
Vinicius Batista <viniciusbatista023@gmail.com>
2019-12-23 08:41:26 -0300

    refatorando movendo o campo de uma classe para outra que faça mais sentido

--- mover_campo/Contato.php
+++ mover_campo/Contato.php
@@ -6,16 +6,34 @@ class Contato
 {
     private $endereco;
     private $telefone;
+    private $email;
 
-    public function __construct(string $endereco, string $telefone)
+    public function __construct(string $endereco, string $telefone, string $email)
     {
         $this->endereco = $endereco;
         $this->telefone = $telefone;
+        $this->email = $email;
+    }
+
+    public function getEendereco(): string
+    {
+        return $this->endereco;
+    }
+
+    public function getTelefone(): string
+    {
+        return $this->telefone;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
     }
 
     public function __toString()
     {
-        return "<p>Endereço: $this->endereco</p>
+        return "<p>Email: $this->email</p>
+                <p>Endereço: $this->endereco</p>
                 <p>Telefone: $this->telefone</p>";
     }
 }
